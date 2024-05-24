b89683d0c058cf9d020d6545fd12fdb5d352c991
TheDepthsAreCalling <18195@rosmini.school.nz>
2024-05-24 21:47:20 +1200

    added background to the main menu

--- .github/Scripts/Menu.gd
+++ .github/Scripts/Menu.gd
@@ -3,9 +3,20 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$VBoxContainer/Start.grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _on_Start_pessed():
-	get_tree().change_scene("res://Scenes/StartGame.tscn")
+
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://Scenes/StartGame.tscn")
+
+
+func _on_controls_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	get_tree().quit()
