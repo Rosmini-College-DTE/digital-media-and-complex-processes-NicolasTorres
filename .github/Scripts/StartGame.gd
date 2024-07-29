ac444518fd641c83b6a22978a66ecf761537b70c
TheDepthsAreCalling <18195@rosmini.school.nz>
2024-07-30 10:50:16 +1200

    animated fire light

--- .github/Scripts/StartGame.gd
+++ .github/Scripts/StartGame.gd
@@ -2,11 +2,12 @@ extends Node2D
 
 @onready var SceneTransitionAnimation = $SceneTransitionAnimation/AnimationPlayer
 @onready var player_camera = $Player/Camera2D
-
+@onready var fire = $AnimationPlayer
 #To change camera 'visibility' limit, make a limit using the help of a marker 2D
 
 func _ready():
 	SceneTransitionAnimation.play("fade_out")
+	fire.play("light")
 	await get_tree().create_timer(1).timeout
 	$SceneTransitionAnimation.hide()
 	player_camera.enabled = true
